81ac365f2773309950d234d3c0477137847f55a3
Voropai_Dmytro <dimon1165@gmail.com>
2019-07-25 11:23:16 +0300

    Started concurrency

---
+++ src/main/java/chapter7/ExcutorMain.java
@@ -0,0 +1,12 @@
+package chapter7;
+
+// Submit task:
+// execute(Runnable); - for non-return value tasks
+// <?>submit(Runnable); <T>Feature<T>submit(Callable<T>) -> returns Feature
+// <T>List<Future<T>>invokeAll(Collection<Callable<? extends T>>) - executes synchronously all tasks form collection
+// returns in the same order Features as submitted.
+// <T> T invokeAny(Collection<Callable<? extends T>>) - executes synchronously all tasks form given collection and
+// returns result on one.
+public class ExcutorMain {
+
+}
